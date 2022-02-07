352c991da37fd8a2d2f641a96be3ab93a7f97fdb
NollChen <99186097+NollChen@users.noreply.github.com>
2022-02-07 14:57:11 +0000

    Delete correlation.R

--- R code/correlation.R
+++
@@ -1,8 +0,0 @@
-
-mydata = read.csv("C:/Users/Noll Chen/OneDrive - Queen's University Belfast/Desktop/airdata2.csv", header = TRUE, stringsAsFactors = F)
-str(mydata)
-dfclean<-mydata[2:6]
-mydata.cor = cor(dfclean)
-install.packages("corrplot")
-library(corrplot)
-corrplot(mydata.cor)
